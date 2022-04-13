9cd38e709df5812d22259e155a02d9ff2858ec88
Admin <tologon.tkbv@gmail.com>
2022-04-13 18:19:39 +0600

    commit

--- console.sql
+++ console.sql
@@ -1,62 +1,37 @@
-create table students(
-    id int primary key ,
-    name varchar(55) not null ,
-    email varchar(55) unique not null ,
-    birth_day date,
-    gender varchar check ( gender = 'F' or gender = 'M')
-
+CREATE TABLE products
+(
+    id SERIAL PRIMARY KEY,
+    productName VARCHAR(30) NOT NULL,
+    company VARCHAR(20) NOT NULL,
+    productCount INT DEFAULT 0,
+    price NUMERIC NOT NULL,
+    isDiscounted BOOL
 );
--- date format=> yy-mm-dd ex: 2022-04-12
--- date format=> hh:ss ex:14:30:00
-insert into students(id, name, email, birth_day, gender)
-values (2, 'Nargiza', 'nargiza@gmail.com', '2025-05-15', 'M');
-insert into students(id, name, email, birth_day, gender)
-values (3, 'Kuba', 'kuba@gmail.com', '2022-05-15', 'F');
 
-select * from students;
+INSERT INTO products (productName, company, productCount, price,
+                      isDiscounted) VALUES('iPhone X', 'Apple',
+                      3, 76000, false),('iPhone 8', 'Apple', 2,
+                      71000, true),('iPhone 7', 'Apple', 5, 42000,
+                      true),('Galaxy S9', 'Samsung', 2, 46000, false),
+                    ('Galaxy S8 Plus', 'Samsung', 1, 56000, true),
+                    ('Desire 12', 'HTC', 5, 28000, true),('Nokia 9',
+                    'HMD Global', 6, 38000, true
+                    );
+    select * from products where company NOT LIKE 'HTC';
+    select * from products where company = 'Apple' and price <75000;
+    select * from products where price >=46000;
+    select * from products ORDER BY price DESC LIMIT 2;
+    select productName from products where price = (select min(price) from products);
+    select * from products where isDiscounted = true;
+    select * from products where isDiscounted = false;
+    select * from products ORDER BY price desc ;
+    select sum (price) from products;
+    select company from products where productCount <= 2;
 
-create table laptop(
-    id int primary key,
-    brand varchar not null,
-    name varchar not null ,
-    memory int not null ,
-    cpu varchar not null
-);
-alter table students add  column  laptop_id int  references laptop(id);
 
-insert into laptops(id, brand, name, memory, cpu)
-values (1, 'Macbbok', 'Air 2020', 265, 'Apple chip'),
-       (2, 'Acer', 'Nitro 5', 512, 'Intel');
 
-select * from laptops;
-alter table laptop rename to laptops;
+--commit
 
-update students
-set laptop_id =1
-where id = 1;
-
-update students
-set name = 'Tologon Tekebaev' where name = 'Tologon';
-
-select  * from  students join laptops on students.laptop_id = laptops.id;
-
-alter table laptops add column student_id int references students(id);
-
-update laptops set student_id = 1 where id = 1;
-
-alter table students drop column laptop_id;
-
-alter table laptops drop column student_id;
-
-create table students_laptops(
-    student_id int references students(id),
-    laptop_id  int references laptops(id)
-);
-insert into students_laptops(student_id, laptop_id)
-values (1, 1),
-       (1, 2);
-select  * from  students s join students_laptops sl on s.id = sl.student_id
-join laptops l on l.id = sl.laptop_id;
 
 
 
